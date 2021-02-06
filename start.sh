72ce3fb1bc09bc18501e3e56d4067110a3249321
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-02-06 20:28:30 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -523,9 +523,11 @@ function containerSetup() {
   }
 # $1=ctTemplate (ubuntu/debian/turnkey-openvpn) - $2=hostname - $3=ContainerRootPasswort - $4=hdd size - $5=cpu cores - $6=RAM Swap/2 - $7=features (keyctl=1,nesting=1,mount=cifs)
   createIDIP
-  echo -e "$info $createlxc $nextCTID - $2"
   if [[ $downloadPath == "local" ]]; then rootfs="local-lvm"; else rootfs=$downloadPath; fi
-  downloadTemplate "$1"
+  template=$1
+  echo -e "Download Template $template"
+  downloadTemplate $template
+  echo -e "$info $createlxc $nextCTID - $2"
   pct create $nextCTID \
     data:vztmpl/$ctTemplate \
     --ostype $ctOstype \
