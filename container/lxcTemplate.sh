bc718816607a5ccecbbf9855ae98df9c0348707a
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-02-07 01:00:10 +0100

    update

--- container/lxcTemplate.sh
+++ container/lxcTemplate.sh
@@ -9,7 +9,7 @@ ctID=$?
 
 # Software that must be installed on the container
 # example - containerSoftware="docker.io docker-compose"
-containerSoftware="docker.io docker-compose"
+containerSoftware=""
 
 # Start Container, because Container stoped aftrer creation
 pct start $ctID
@@ -26,13 +26,7 @@ for package in $containerSoftware; do
 done
 
 # Execute commands on containers
-pct exec $ctID -- bash -ci "systemctl start docker && systemctl enable docker > /dev/null 2>&1"
-pct exec $ctID -- bash -ci "mkdir -p /root/npm"
-pct exec $ctID -- bash -ci "wget -qO /root/npm/config.json $rawGitHubURL/container/$ctName/config.json"
-pct exec $ctID -- bash -ci "wget -qO /root/npm/docker-compose.yml $rawGitHubURL/container/$ctName/docker-compose.yml"
-pct exec $ctID -- bash -ci "sed -i 's+ROOTPASSWORDTOCHANGE+$ctRootpw+g' /root/npm/config.json"
-pct exec $ctID -- bash -ci "sed -i 's+ROOTPASSWORDTOCHANGE+$ctRootpw+g' /root/npm/docker-compose.yml"
-pct exec $ctID -- bash -ci "cd npm && docker-compose up -d --quiet-pull > /dev/null 2>&1"
+pct exec $ctID -- bash -ci ""
 
 # Container description in the Proxmox web interface
 pct set $ctID --description $'Shell Login\nBenutzer: root\nPasswort: '"$ctRootpw"$'\n\nWebGUI\nAdresse: http://'"$nextCTIP"$':81\nBenutzer: admin@example.com\nPasswort: changeme'
@@ -40,6 +34,7 @@ pct set $ctID --description $'Shell Login\nBenutzer: root\nPasswort: '"$ctRootpw
 # echo [INFO] Create firewall rules for container "CONTAINERNAME"
 echo -e "$info $lng_lxcfw \"$ctName\""
 
+# Creates firewall rules for the container
 # Create Firewallgroup - If a port should only be accessible from the local network - IN ACCEPT -source +network -p tcp -dport PORTNUMBER -log nolog
 echo -e "[group $(echo $ctName|tr "[:upper:]" "[:lower:]")]\n\nIN HTTPS(ACCEPT) -log nolog\nIN HTTP(ACCEPT) -log nolog\nIN ACCEPT -source +network -p tcp -dport 81 -log nolog # Weboberfläche\n\n" >> $clusterfile
 
