1ee30e015809da4a01f3ff24ac76c8d27bc10de5
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-02-10 01:44:31 +0100

    Update install.sh

--- container/nextcloud/install.sh
+++ container/nextcloud/install.sh
@@ -2,14 +2,14 @@
 
 # Container Configuration
 # $1=ctTemplate (ubuntu/debian/turnkey-openvpn) - $2=hostname - $3=ContainerRootPasswort - $4=hdd size - $5=cpu cores - $6=RAM Swap/2 - $7=unprivileged 0/1 - $8=features (keyctl=1,nesting=1,mount=cifs)
-containerSetup ubuntu $ctName $ctRootpw 8 1 4096 0 "mount=cifs;nfs"
+containerSetup ubuntu $ctName $ctRootpw 8 1 4096 0 "nesting=1,mount=cifs;nfs"
 
 # Comes from Mainscript - start.sh --> Function containerSetup
 ctID=$?
 
 # Software that must be installed on the container
 # example - containerSoftware="docker.io docker-compose"
-containerSoftware="apache2 mariadb-server libapache2-mod-php7.4 php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl php7.4-gmp php7.4-bcmath php-imagick php7.4-xml php7.4-zip"
+containerSoftware="apache2 mariadb-server unzip smbclient redis-server php-redis php-apcu libapache2-mod-php7.4 php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl php7.4-gmp php7.4-bcmath php-imagick php7.4-xml php7.4-zip"
 
 # Start Container, because Container stoped aftrer creation
 pct start $ctID
@@ -29,35 +29,29 @@ done
 pct exec $ctID -- bash -ci "sed -i 's+""memory_limit = 128M""+""memory_limit = 1024M""+g' /etc/php/7.4/apache2/php.ini"
 pct exec $ctID -- bash -ci "sed -i 's+""upload_max_filesize = 2M""+""upload_max_filesize = 16G""+g' /etc/php/7.4/apache2/php.ini"
 pct exec $ctID -- bash -ci "sed -i 's+""post_max_size = 8M""+""post_max_size = 16G""+g' /etc/php/7.4/apache2/php.ini"
-pct exec $ctID -- bash -ci "sed -i 's+"";date.timezone = ""+""date.timezone = Europe/Berlin""+g' /etc/php/7.4/apache2/php.ini"
-pct exec $ctID -- bash -ci "wget -qO /root/sql_secure.sh $rawGitHubURL/container/nextcloud/sqlsecure.sh"
-pct exec $ctID -- bash -ci "sed -i 's+ROOTPASSWORDTOCHANGE+$ctRootpw+g' /root/sql_secure.sh"
-pct exec $ctID -- bash -ci "bash /root/sql_secure.sh"
-pct exec $ctID -- bash -ci "rm /root/sql_secure.sh"
-pct exec $ctID -- bash -ci "mysql -u root -p$ctRootpw -e ""CREATE DATABASE nextcloud;"" > /dev/null 2>&1" > EOF
-pct exec $ctID -- bash -ci "mysql -u root -p$ctRootpw -e ""CREATE USER nextcloud@localhost IDENTIFIED BY '${ctRootpw}';"" > /dev/null 2>&1" > EOF
-pct exec $ctID -- bash -ci "mysql -u root -p$ctRootpw -e ""GRANT ALL PRIVILEGES ON nextcloud.* TO nextcloud@localhost;"" > /dev/null 2>&1" > EOF
-pct exec $ctID -- bash -ci "mysql -u root -p$ctRootpw -e ""FLUSH PRIVILEGES;"" > /dev/null 2>&1" > EOF
-pct exec $ctID -- bash -ci "wget https://download.nextcloud.com/server/releases/latest.zip"
-pct exec $ctID -- bash -ci "unzip latest.zip > /dev/null 2>&1"
-pct exec $ctID -- bash -ci "mv nextcloud /var/www/ > /dev/null 2>&1"
-pct exec $ctID -- bash -ci "rm latest.zip"
+if [[ $lang == "de" ]]; then
+  pct exec $ctID -- bash -ci "sed -i 's+"";date.timezone = ""+""date.timezone = Europe/Berlin""+g' /etc/php/7.4/apache2/php.ini"
+fi
+wget -rqO /root/sqlsecure.conf $rawGitHubURL/config/sqlsecure.conf
+source /root/sqlsecure.conf
+sqlSecure
+pct exec $ctID -- bash -ci "echo \"create database nextcloud;grant all on nextcloud.* to 'nextcloud'@'localhost' identified by '${ctRootpw}';flush privileges;\"|mysql"
+pct exec $ctID -- bash -ci "wget -q https://download.nextcloud.com/server/releases/latest.zip"
+pct exec $ctID -- bash -ci "unzip latest.zip > /dev/null 2>&1 && rm latest.zip"
+pct exec $ctID -- bash -ci "mv nextcloud /var/www/"
+pct exec $ctID -- bash -ci "sed -i 's+supervised no+supervised systemd+g' /etc/redis/redis.conf > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "systemctl restart redis"
 pct exec $ctID -- bash -ci "wget -qO /etc/apache2/sites-available/nextcloud.conf $rawGitHubURL/container/nextcloud/nextcloud.conf"
-pct exec $ctID -- bash -ci "sed -i 's+EMAILTOCHANGE+$varrootmail+g' /etc/apache2/sites-available/nextcloud.conf > /dev/null 2>&1"
-pct exec $ctID -- bash -ci "sed -i 's+SERVERNAMETOCHANGE+cloud.$(hostname -f | cut -d. -f2,3,4,5)+g' /etc/apache2/sites-available/nextcloud.conf > /dev/null 2>&1"
-pct exec $ctID -- bash -ci "sed -i 's+SERVERALIESTOCHANGE+cloud.$(echo "$varrootmail" | cut -d\@ -f2)+g' /etc/apache2/sites-available/nextcloud.conf > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "sed -i 's+EMAILTOCHANGE+'"$varrootmail"$'+g' /etc/apache2/sites-available/nextcloud.conf > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "sed -i 's+SERVERNAMETOCHANGE+'"$nextCTIP"$'+g' /etc/apache2/sites-available/nextcloud.conf > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "sed -i 's+SERVERALIESTOCHANGE+cloud.'"$(echo \"$varrootmail\" | cut -d\@ -f2)"$'+g' /etc/apache2/sites-available/nextcloud.conf > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "a2ensite nextcloud.conf > /dev/null 2>&1 && a2enmod rewrite > /dev/null 2>&1 && a2enmod headers > /dev/null 2>&1 && a2enmod env > /dev/null 2>&1 && a2enmod dir > /dev/null 2>&1 && a2enmod mime > /dev/null 2>&1"
-pct exec $ctID -- bash -ci "systemctl restart apache2.service"
 pct exec $ctID -- bash -ci "chmod -R 755 /var/www/nextcloud/"
 pct exec $ctID -- bash -ci "chown -R www-data:www-data /var/www/nextcloud/"
-
-# If NAS exist in Network, bind to Container, only privileged and mount=cifs Feature is set
-if [[ $nasexists == "y" ]]; then
-  lxcMountNAS $ctID
-fi
+pct exec $ctID -- bash -ci "systemctl restart apache2"
 
 # Container description in the Proxmox web interface
-pct set $ctID --description $'Shell Login\nBenutzer: root\nPasswort: '"$ctRootpw"$'\n\nWebGUI\nAdresse: https://'"$nextCTIP"
+pct set $ctID --description $'Shell Login\nBenutzer: root\nPasswort: '"$ctRootpw"$'\n\nWebGUI\nAdresse: https://'"$nextCTIP""$nasFolder"
 
 # echo [INFO] Create firewall rules for container "CONTAINERNAME"
 echo -e "$info $lng_lxcfw \"$ctName\""
