db1641614270840a9385275a1fb08863172c41f1
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-05-09 02:45:47 +0200

    Update configLXC.sh

--- configLXC.sh
+++ configLXC.sh
@@ -368,7 +368,7 @@ for hostname in $var_lxcchoice; do
     yesno=$?
     # Rename existing Container with same Name
     if [ $yesno -eq 0 ]; then
-      newName=$(whiptail --inputbox --nocancel --backtitle "© 2021 - SmartHome-IoT.net - ${lng_wrd_container} ${lng_wrd_configuration}" --title "${lng_wrd_container}" "\n${lng_ask_lxc_rename}" 20 80 10 "${hostname}-old" 3>&1 1>&2 2>&3)
+      newName=$(whiptail --inputbox --nocancel --backtitle "© 2021 - SmartHome-IoT.net - ${lng_wrd_container} ${lng_wrd_configuration}" --title "${lng_wrd_container}" "\n${lng_ask_lxc_rename}\n\n${lng_txt_hostname}" 20 80 10 "${hostname}-old" 3>&1 1>&2 2>&3)
       # Check if $newName is a valid Hostname containe only upper and lower case letters and/or digits if it is skip Container creation
       if [[ $newName =~ ^[A-Za-z0-9-]+$ ]] && [[ $newName == *[ÄäÖöÜüß]* ]]; then
         pct set $(pct list | grep -w $hostname | awk '{print $1}') --Hostname $newName > /dev/null 2>&1
