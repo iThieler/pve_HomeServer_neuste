e17b9ffbc83b20f84c54a34952e166fdf161fbb4
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-07-26 08:25:42 +0200

    Update global_functions.sh

--- handler/global_functions.sh
+++ handler/global_functions.sh
@@ -8,3 +8,8 @@ function pingIP() {
   fi
 }
 
+function githubLatest() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
