e80f9ae61ac4ca38a02d8526c963caa577e5aa8e
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-07-29 20:58:23 +0200

    Update variables.sh

--- bin/variables.sh
+++ bin/variables.sh
@@ -5,3 +5,19 @@ shiot_configFile=".config.sh"
 pve_majorversion=$(pveversion | cut -d/ -f2 | cut -d. -f1)
 pve_version=$(pveversion | cut -d/ -f2 | cut -d- -f1)
 networkIP=$(ip -o -f inet addr show | awk '/scope global/ {print $4}' | cut -d/ -f1 | cut -d. -f1,2,3)
+
+# Network Variables
+gatewayIP=$(ip r | grep default | cut -d" " -f3)
+pve_ip=$(ip -o -f inet addr show | awk '/scope global/ {print $4}' | cut -d/ -f1)
+cidr=$(ip -o -f inet addr show | awk '/scope global/ {print $4}' | cut -d/ -f2)
+networkIP=$(ip -o -f inet addr show | awk '/scope global/ {print $4}' | cut -d/ -f1 | cut -d. -f1,2,3)
+publicIP=$(dig @resolver4.opendns.com myip.opendns.com +short)
+pve_fqdn=$(hostname -f)
+pve_hostname=$(hostname)
+
+# Proxmox Variables
+clusterfileFW="/etc/pve/firewall/cluster.fw"
+hostfileFW="/etc/pve/nodes/$pve_hostname/host.fw"
+pve_timezone=$(timedatectl | grep "Time zone" | awk '{print $3}')
+pve_osname=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d= -f2)
+
