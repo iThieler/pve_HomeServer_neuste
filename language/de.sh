afb2ab427a13f5c2cc7cbadcf167eb22ef551949
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-07-30 22:48:15 +0200

    Update de.sh

--- language/de.sh
+++ language/de.sh
@@ -7,11 +7,18 @@ btn_3="JA"
 btn_4="NEIN"
 btn_5="RECOVER"
 btn_6="KONFIG"
+btn_7="PRÜFEN"
 
 # Titles
 tit_1="ERROR"
 tit_2="NAS"
 tit_3="NETZWERKROBOTER"
+tit_4="VLAN"
+tit_5="MAILSERVER"
+
+# Words
+wrd_1="Benutzername"
+wrd_2="Passwort"
 
 # start.sh
 txt_0001="Leider konnte das Standard Konfigurationsverzeichnis nicht gefunden werden."
@@ -25,6 +32,25 @@ txt_0008="Wie lautet das Passwort von"
 txt_0009="Konfigurationsdatei erfolgreich kopiert"
 
 # handler/generate_config.sh
+txt_0051="Wie lautet der Benutzername, den Du deinem Netzwerkroboter zugewiesen hast?"
+txt_0052="Wie lautet das Passwort, welches Du deinem Netzwerkroboter zugewiesen hast?"
+txt_0053="Wenn Du hier kein Passwort eingibst, wird automatisch ein sicheres 26-Zeichen langes Passwort erstellt."
+txt_0054="Erstelle einen Benutzer auf deiner NAS mit folgenden Daten und weise diesem Benutzer Administratorrechte zu."
+txt_0055="Auf deiner NAS werden 2 Freigegebene Ordner benötigt, wenn nicht vorhanden, erstelle diese und weise deinem Netzwerkroboter Lese-/Schreibrachte zu, bevor du fortfährst."
+txt_0056="Benutzt Du virtuelle Netzwerke (VLANs)?"
+txt_0057="Welche VLAN-ID nutzt Du für dein Server Netzwerk?"
+txt_0058="Welche VLAN-ID nutzt Du für dein SmartHome Netzwerk?"
+txt_0059="Welche VLAN-ID nutzt Du für dein Gäste Netzwerk?"
+txt_0060="An welche E-Mailadresse sollen benachrichtigungen von deinem Server gesendet werden?"
+txt_0061="Wie lautet die Adresse deines Postausgangsserver?"
+txt_0062="Welcher Port wird für deinen verwendet?"
+txt_0063="Benötigt dein Mailserver für den Login TLS/STARTTLS?"
+txt_0064="Welcher Benutzername wird für den Login am Mailserver verwendet?"
+txt_0065="Wie lautet das Passwort, welches für den Login am Mailserver verwendet wird?"
+txt_0066="Wie lautet die Absendeadresse, von der deine E-Mails versendet werden sollen?"
+txt_0067="Nutzt Du eine NAS in deinem Netzwerk?"
+txt_0068="Wie lautet die IP-Adresse deiner NAS?"
+txt_0069="Sollen deine Passwörter unverschlüsselt im Klartext in der Konfigurationsdatei gespeichert werden?"
 
 
 
