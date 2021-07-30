9b5368f8b50b986238858a52736f0c0336810f1e
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-07-30 06:07:51 +0200

    Update de.sh

---
+++ language/de.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Buttons
+btn_1="OK"
+btn_2="ABBRECHEN"
+btn_3="JA"
+btn_4="NEIN"
+btn_5="RECOVER"
+btn_6="KONFIG"
+
+# Titles
+tit_1="ERROR"
+tit_2="NAS"
+tit_3="NETZWERKROBOTER"
+
+# start.sh
+txt_0001="Leider konnte das Standard Konfigurationsverzeichnis nicht gefunden werden."
+txt_0002="Es sieht so aus, das es sich bei diesem Server um eine Neuinstallation handelt."
+txt_0003="Soll dieser Server neu konfiguriert werden, oder möchtest du eine zuvor gesicherte Konfigurationsdatei von deinem NAS laden (Recovery)?"
+txt_0004="Wie lautet die IP-Adresse deiner NAS?"
+txt_0005="Wie heißt der Ordner, in dem die Datei zu finden ist (ohne \"\\\" am Anfang oder am Ende)?"
+txt_0006="Wie heißt die Datei, die die Konfigurationsvariablen enthält?"
+txt_0007="Wie lautet der Benutzername des Benutzers der Leserechte auf deiner NAS hat?"
+txt_0008="Wie lautet das Passwort von"
+txt_0009="Konfigurationsdatei erfolgreich kopiert"
+
+# handler/generate_config.sh
+
+
+
+
