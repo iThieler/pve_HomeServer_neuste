8983410d825afa747a340d3fe30670a7e688ed49
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-04 18:11:54 +0200

    Update variables.sh

--- bin/variables.sh
+++ bin/variables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# General Variables
 shiot_configPath="/opt/smarthome-iot_net/"
 shiot_configFile=".config.sh"
 pve_majorversion=$(pveversion | cut -d/ -f2 | cut -d. -f1)
@@ -20,3 +21,17 @@ clusterfileFW="/etc/pve/firewall/cluster.fw"
 hostfileFW="/etc/pve/nodes/$pve_hostname/host.fw"
 pve_timezone=$(timedatectl | grep "Time zone" | awk '{print $3}')
 pve_osname=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d= -f2)
+
+# Hardware Variables
+rootDisk=$(lsblk -oMOUNTPOINT,PKNAME -P | grep 'MOUNTPOINT="/"' | cut -d' ' -f2 | cut -d\" -f2 | sed 's#[0-9]*$##')
+
+# search second Harddisk and check if is SSD
+if [[ $(cat /sys/block/$(lsblk -nd --output NAME | grep "s" | sed "s#$rootDisk##" | sed ':M;N;$!bM;s#\n##g')/queue/rotational) -eq 0 ]]; then
+  secondDisk=$(lsblk -nd --output NAME | grep "s" | sed "s#$rootDisk##" | sed ':M;N;$!bM;s#\n##g')
+  ctTemplateDisk="data"
+else
+  ctTemplateDisk="local"
+fi
+
+# check if Variable is valid URL
+regexURL='^(https?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]\.[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]$'  # if [[ ! $URL =~ $regexURL ]]; then; fi
