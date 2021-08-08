13f4d831a8abe1d356e1bda1890bdf890dba6025
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-08 23:27:49 +0200

    Update generate_lxc.sh

--- handler/generate_lxc.sh
+++ handler/generate_lxc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source "$script_path/bin/variables.sh"
+source "/root/pve_HomeServer/bin/variables.sh"
 source "$script_path/bin/var_containerOS.sh"
 source "$script_path/handler/global_functions.sh"
 source "$shiot_configPath/$shiot_configFile"
