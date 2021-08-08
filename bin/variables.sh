3f9fa9a2eb61a1a460aff0dcd22a1ff537e4a323
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-08 22:51:48 +0200

    Update variables.sh

--- bin/variables.sh
+++ bin/variables.sh
@@ -3,6 +3,7 @@
 # General Variables
 shiot_configPath="/opt/smarthome-iot_net"
 shiot_configFile=".config.sh"
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pve_majorversion=$(pveversion | cut -d/ -f2 | cut -d. -f1)
 pve_version=$(pveversion | cut -d/ -f2 | cut -d- -f1)
 networkIP=$(ip -o -f inet addr show | awk '/scope global/ {print $4}' | cut -d/ -f1 | cut -d. -f1,2,3)
