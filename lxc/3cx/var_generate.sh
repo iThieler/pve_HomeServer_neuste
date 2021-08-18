0d235be4545183e50b3eb881bc0961b57f9817f2
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-18 09:57:21 +0200

    Update var_generate.sh

--- lxc/3cx/var_generate.sh
+++ lxc/3cx/var_generate.sh
@@ -1,2 +1,42 @@
 #!/bin/bash
 
+################### Container Configuration ###################
+
+template=${osDebian9}
+hddsize=32
+cpucores=1
+memory=1024
+swap=1024
+unprivileged=0
+features="mount=cifs;nfs"
+description="${desc_001}"
+
+#################### WebGUI Configuration #####################
+
+webgui=true
+webguiName=( "WebGUI (Admin)" "WebGUI (${lng_wrd_user})" "WebGUI (${desc_002})" )
+webguiPort=( "5001" "5001/webclient" "5015?v=2" )
+webguiPath=( "" "" "" )
+webguiUser=( "" "${desc_003}" "" )
+webguiPass=( "" "${desc_004}" "" )
+webguiProt=( "http" "http" "http" )
+
+################### Firewall Configuration ####################
+
+fwPort=( "5015" "5001" "5062" "5062" "5063" "5090" "5090" "9000:10999" )
+fwNetw=( "network" "pnetwork" "" "" "" "" "" "" )
+fwProt=( "tcp" "tcp" "udp" "tcp" "tcp" "udp" "tcp" "udp" )
+fwDesc=( "${fw_001}" "${fw_002}" "${fw_003}" "${fw_003}" "${fw_004}" "${fw_005}" "${fw_005}" "${fw_006}" )
+
+#################### Needed Hardwarebinds #####################
+
+nasneeded=false
+dvbneeded=false
+vganeeded=false
+
+####################### Needed Services #######################
+
+smtpneeded=false
+apparmorProfile=""
+sambaneeded=false
+sambaUser=""
