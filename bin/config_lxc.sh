f81c6ed856ab6c8674836c664e13d8b401ddc667
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-18 20:18:25 +0200

    Update config_lxc.sh

--- bin/config_lxc.sh
+++ bin/config_lxc.sh
@@ -10,18 +10,18 @@ ctID=$1
 containername="$(pct list | grep $ctID | awk '{print $3}')"
 
 # Load container language file if not exist load english language
-if "$script_path/lxc/$containername/language/$var_language.sh"; then
+if [ -f "$script_path/lxc/$containername/language/$var_language.sh" ]; then
   source "$script_path/lxc/$containername/language/$var_language.sh"
 else
   source "$script_path/lxc/$containername/language/en.sh"
 fi
 
 # Load special functions for Container config
-if "$script_path/lxc/$containername/functions.sh"; then
+if [ -f "$script_path/lxc/$containername/functions.sh" ]; then
   source "$script_path/lxc/$containername/functions.sh"
 fi
 
-# Ask for SMTP-Password if SMTP is needed
+# Ask for SMTP-Password if SMTP is needed and Passwort is not save in shiot_configFile
 if $smtpneeded; then
   if [ -z "$var_mailpassword" ]; then
     var_mailpassword=$(whiptail --passwordbox --ok-button " ${btn_1} " --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ${tit_5} " "\n${txt_0065}" 10 80 3>&1 1>&2 2>&3)
@@ -66,7 +66,7 @@ if [ -z "$var_nasip" ] && $nasneeded; then
     pct exec $ctID -- bash -ci "echo -e \"username=$var_robotname\npassword=$var_robotpw\" > /home/.smbbackup"
     pct exec $ctID -- bash -ci "mount -a"
   else
-    echo "-- Es kann keine NAS an den Container gebunden werden, da keine konfiguriert ist"
+    echo "-- $txt_0251"
   fi
 fi
 
@@ -81,9 +81,9 @@ if $sambaneeded; then
     pct exec $ctID -- bash -ci "mkdir -p /root/sambashare/$user"
     pct exec $ctID -- bash -ci "echo -e \"\n[$user]\ncomment = Sambashare\npath = /root/sambashare/$user\nwrite list = $user\nvalid users = $user\nforce user = smb\" >> /etc/samba/smb.conf"
     if [ -z "$smbuserdesc" ]; then
-      smbuserdesc="#$lng_wrd_user:   $user\n#$lng_wrd_password:   $smbpasswd"
+      smbuserdesc="#$wrd_8:   $user\n#$wrd_2:   $smbpasswd"
     else
-      smbuserdesc="${smbuserdesc}\n#$lng_wrd_user:   $user\n#$lng_wrd_password:   $smbpasswd"
+      smbuserdesc="${smbuserdesc}\n#$wrd_8:   $user\n#$wrd_2:   $smbpasswd"
     fi
   done
   pct exec $ctID -- bash -ci "sed -i 's#map to guest = bad user#map to guest = never#' /etc/samba/smb.conf > /dev/null 2>&1"
@@ -92,15 +92,16 @@ if $sambaneeded; then
 fi
 
 # Update/Upgrade Container
-echo "-- Der Container wird aktualisiert"
+echo "-- $txt_0252"
 pct exec $ctID -- bash -ci "apt-get update > /dev/null 2>&1 && apt-get upgrade -y > /dev/null 2>&1"
 
 # Install Container Standardsoftware
-echo "-- Containersoftware wird installiert"
+echo "-- $txt_0253"
 pct exec $ctID -- bash -ci "apt-get install -y curl wget software-properties-common apt-transport-https lsb-release gnupg2 net-tools > /dev/null 2>&1"
 
 # Commands that are executed in the container
 if [ -n "$lxcCommands" ]; then
+  echo "-- $txt_0254"
   IFS=$'\n'
   for command in $lxcCommands; do
     pct exec $ctID -- bash -ci "$command"
@@ -110,6 +111,7 @@ fi
 
 # Commands to be executes in the Host (Proxmox) shell after complete Container creation
 if [ -n "$pveCommands" ]; then
+  echo "-- $txt_0255"
   IFS=$'\n'
   for command in $pveCommands; do
     $command
@@ -118,14 +120,14 @@ if [ -n "$pveCommands" ]; then
 fi
 
 # Create Container description, you can find it on Proxmox WebGUI
-echo "-- Die Containererstellung wird abgeschlossen"
+echo "-- $txt_0256"
 lxcConfigFile="/etc/pve/lxc/$ctID.conf"
 lxcConfigOld=$(cat $lxcConfigFile)
 
 if [ -n "$description" ]; then
-  echo -e "#>> Shell <<\n#$lng_wrd_user:   root\n#$lng_wrd_password:   $ctRootPW" > $lxcConfigFile
+  echo -e "#>> Shell <<\n#$wrd_8:   root\n#$wrd_2:   $ctRootPW" > $lxcConfigFile
 else
-  echo -e "#${description}\n#\n#>> Shell <<\n#$lng_wrd_user:   root\n#$lng_wrd_password:   $ctRootPW" > $lxcConfigFile
+  echo -e "#${description}\n#\n#>> Shell <<\n#$wrd_8:   root\n#$wrd_2:   $ctRootPW" > $lxcConfigFile
 fi
 
 if $webgui; then
@@ -134,49 +136,53 @@ if $webgui; then
     if [[ ! ${webguiPath[i]} == "" ]]; then webguiAdress="${webguiAdress}${webguiPath[i]}"; fi
     if [[ ! ${webguiName[i]} == "" ]]; then
       if [ $i -lt 1 ]; then
-        echo -e "#\n#>> ${webguiName[i]} <<\n#$lng_wrd_webadress:   $webguiAdress" >> $lxcConfigFile
+        echo -e "#\n#>> ${webguiName[i]} <<\n#$wrd_9:   $webguiAdress" >> $lxcConfigFile
       else
-        echo -e "#>> ${webguiName[i]} <<\n#$lng_wrd_webadress:   $webguiAdress" >> $lxcConfigFile
+        echo -e "#>> ${webguiName[i]} <<\n#$wrd_9:   $webguiAdress" >> $lxcConfigFile
       fi
     fi
-    if [[ ! ${webguiUser[i]} == "" ]]; then echo -e "#$lng_wrd_user:   ${webguiUser[i]}" >> $lxcConfigFile; fi
-    if [[ ! ${webguiPass[i]} == "" ]]; then echo -e "#$lng_wrd_password:   ${webguiPass[i]}" >> $lxcConfigFile; fi
+    if [[ ! ${webguiUser[i]} == "" ]]; then echo -e "#$wrd_8:   ${webguiUser[i]}" >> $lxcConfigFile; fi
+    if [[ ! ${webguiPass[i]} == "" ]]; then echo -e "#$wrd_2:   ${webguiPass[i]}" >> $lxcConfigFile; fi
   done
 fi
 
 if [ -n "$var_nasip" ] && $nasneeded; then
-  echo -e "#\n#>> $lng_wrd_nas <<\n#$lng_wrd_mediafolder:   /media\n#$lng_wrd_backupfolder:   /mnt/backup" >> $lxcConfigFile
+  echo -e "#\n#>> $wrd_10 <<\n#$wrd_11:   /media\n#$wrd_12:   /mnt/backup" >> $lxcConfigFile
 fi
 
 if $sambaneeded; then
-  echo -e "#\n#>> Samba (smb) <<\n#Windows-$lng_wrd_sharedfolder:   \\\\\\$networkIP.$ctIP\n#Mac-$lng_wrd_sharedfolder:       smb://$networkIP.$ctIP\n#Linux-$lng_wrd_sharedfolder:     smb://$networkIP.$ctIP" >> $lxcConfigFile
+  echo -e "#\n#>> Samba (smb) <<\n#Windows-$wrd_13:   \\\\\\$networkIP.$ctIP\n#Mac-$wrd_13:       smb://$networkIP.$ctIP\n#Linux-$wrd_13:     smb://$networkIP.$ctIP" >> $lxcConfigFile
   echo -e "$smbuserdesc" >> $lxcConfigFile
 fi
 echo -e "$lxcConfigOld" >> $lxcConfigFile
 
 # Create Firewall Group and Rules for Container
-echo "Die Proxmox Firewall wird für den Container konfiguriert"
+echo "-- $txt_0257"
 clusterfileFW="/etc/pve/firewall/cluster.fw"
-echo -e "\n[group $(echo fwsg_$hostname_lxc | tr "[:upper:]" "[:lower:]")]" >> $clusterfileFW    # This Line will create the Firewall Goup Containername - don't change it
+if [ $(cat $clusterfileFW | grep -cw fwsg_$hostname_lxc) -eq 0 ]; then
+  echo -e "\n[group $(echo fwsg_$hostname_lxc | tr "[:upper:]" "[:lower:]")]" >> $clusterfileFW    # This Line will create the Firewall Goup Containername - don't change it
 
-if $sambaneeded; then
-  echo -e "IN ACCEPT -source +network -p tcp -dport 445 -log nolog # Samba (smb)" >> $clusterfileFW
-  echo -e "IN ACCEPT -source +network -p tcp -dport 137 -log nolog # Samba (NetBios/Name resolution)" >> $clusterfileFW
-  echo -e "IN ACCEPT -source +network -p udp -dport 137 -log nolog # Samba (NetBios/Name resolution)" >> $clusterfileFW
-  echo -e "IN ACCEPT -source +network -p udp -dport 138 -log nolog # Samba (NetBios/Name resolution)" >> $clusterfileFW
-  echo -e "IN ACCEPT -source +network -p tcp -dport 139 -log nolog # Samba (NetBios/Name resolution)" >> $clusterfileFW
+  if $sambaneeded; then
+    echo -e "IN ACCEPT -source +network -p tcp -dport 445 -log nolog # Samba (smb)" >> $clusterfileFW
+    echo -e "IN ACCEPT -source +network -p tcp -dport 137 -log nolog # Samba (NetBios/Name resolution)" >> $clusterfileFW
+    echo -e "IN ACCEPT -source +network -p udp -dport 137 -log nolog # Samba (NetBios/Name resolution)" >> $clusterfileFW
+    echo -e "IN ACCEPT -source +network -p udp -dport 138 -log nolog # Samba (NetBios/Name resolution)" >> $clusterfileFW
+    echo -e "IN ACCEPT -source +network -p tcp -dport 139 -log nolog # Samba (NetBios/Name resolution)" >> $clusterfileFW
+  fi
+
+  for ((i=0;i<=${#fwPort[@]};i++)); do
+    if [[ ${fwNetw[i]} == "" ]]; then fwnw=""; else fwnw=" -source +${fwNetw[i]}"; fi
+    if [[ ${fwDesc[i]} == "" ]]; then fw_desc=""; else fw_desc=" # ${fwDesc[i]}"; fi
+    echo -e "IN ACCEPT$fwnw -p ${fwProt[i]} -dport ${fwPort[i]} -log nolog$fw_desc" >> $clusterfileFW
+  done
 fi
 
-for ((i=0;i<=${#fwPort[@]};i++)); do
-  if [[ ${fwNetw[i]} == "" ]]; then fwnw=""; else fwnw=" -source +${fwNetw[i]}"; fi
-  if [[ ${fwDesc[i]} == "" ]]; then fw_desc=""; else fw_desc=" # ${fwDesc[i]}"; fi
-  echo -e "IN ACCEPT$fwnw -p ${fwProt[i]} -dport ${fwPort[i]} -log nolog$fw_desc" >> $clusterfileFW
-done
-
-echo -e "[OPTIONS]\n\nenable: 1\n\n[RULES]\n\nGROUP fwsg_$(echo $hostname_lxc|tr "[:upper:]" "[:lower:]")" > /etc/pve/firewall/$ctID.fw    # Allow generated Firewallgroup, don't change it
+if [ $(cat /etc/pve/firewall/$ctID.fw | grep -cw fwsg_$hostname_lxc) -eq 0 ]; then
+  echo -e "[OPTIONS]\n\nenable: 1\n\n[RULES]\n\nGROUP fwsg_$(echo $hostname_lxc | tr "[:upper:]" "[:lower:]")" > /etc/pve/firewall/$ctID.fw    # Allow generated Firewallgroup, don't change it
+fi
 
 # Cleanup Container History an reboot
-echo "Lösche Verlaufsdaten im Container"
+echo "-- $txt_0258"
 pct exec $ctID -- bash -ci "cat /dev/null > ~/.bash_history"
 pct exec $ctID -- bash -ci "history -c"
 pct exec $ctID -- bash -ci "history -w"
