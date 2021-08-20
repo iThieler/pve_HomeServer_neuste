1a4c84bc068eb0fa7d8ab668cda77bf4cf059951
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-20 23:55:06 +0200

    Update config.sh

--- lxc/piHole/config.sh
+++ lxc/piHole/config.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-Commands="pct exec $ctID -- bash -ci \"mkdir -p /etc/pihole/\"
-          pct push $ctIP \"$script_path/lxc/piHole/setupVars.conf\" \"/etc/pihole/setupVars.conf\"
-          pct exec $ctID -- bash -ci \"sed -i 's#IPADRESSTOCHANGE#'"$ctIP"'#g' /etc/pihole/setupVars.conf\"
-          pct exec $ctID -- bash -ci \"sed -i 's#CIDRTOCHANGE#'"$cidr"'#g' /etc/pihole/setupVars.conf\"
-          pct exec $ctID -- bash -ci \"curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended > /dev/null 2>&1\"
-          pct exec $ctID -- bash -ci \"/usr/local/bin/pihole -a -p changeme > /dev/null 2>&1\"
-          pct push $ctIP \"$script_path/lxc/piHole/updateAdlist.sh\" \"/root/updateAdlist.sh\"
-          pct exec $ctID -- bash -ci \"chmod +x /root/updateAdlist.sh\"
-          pct exec $ctID -- bash -ci \"bash /root/updateAdlist.sh\"
-          pct exec $ctID -- bash -ci \"crontab -l | { cat; echo \"0 03 1,14 * *   root    /root/updateAdlist.sh\"; } | crontab -\""
+pct exec $1 -- bash -ci "mkdir -p /etc/pihole/"
+pct push $1 "$script_path/lxc/piHole/setupVars.conf" "/etc/pihole/setupVars.conf"
+pct exec $1 -- bash -ci "sed -i 's#IPADRESSTOCHANGE#'"$ctIP"'#g' /etc/pihole/setupVars.conf"
+pct exec $1 -- bash -ci "sed -i 's#CIDRTOCHANGE#'"$cidr"'#g' /etc/pihole/setupVars.conf"
+pct exec $1 -- bash -ci "curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended > /dev/null 2>&1"
+pct exec $1 -- bash -ci "/usr/local/bin/pihole -a -p changeme > /dev/null 2>&1"
+pct push $1 "$script_path/lxc/piHole/updateAdlist.sh" "/root/updateAdlist.sh"
+pct exec $1 -- bash -ci "chmod +x /root/updateAdlist.sh"
+pct exec $1 -- bash -ci "bash /root/updateAdlist.sh"
+pct exec $1 -- bash -ci "crontab -l | { cat; echo \"0 03 1,14 * *   root    /root/updateAdlist.sh\"; } | crontab -"
+
+exit 0
