37041fca72af077716ea506bce66938329f58378
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-21 01:53:38 +0200

    Update config.sh

--- lxc/ReverseProxy/config.sh
+++ lxc/ReverseProxy/config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ctID=$1
-ctRootpw=$2
-ctIP=$(lxc-info $ctID -iH | grep $networkIP)
-containername=$(pct list | grep 100 | awk '{print $3}')
-
 source "$script_path/bin/variables.sh"
 source "$script_path/handler/global_functions.sh"
 source "$shiot_configPath/$shiot_configFile"
 source "$script_path/language/$var_language.sh"
 source "$script_path/lxc/$containername/language/$var_language.sh"
+
+ctID=$1
+ctRootpw=$2
+ctIP=$(lxc-info $ctID -iH | grep $networkIP)
+containername=$(pct list | grep 100 | awk '{print $3}')
 
 pct exec $ctID -- bash -ci "apt-get install -y docker.io docker-compose > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "systemctl start docker && systemctl enable docker > /dev/null 2>&1"
