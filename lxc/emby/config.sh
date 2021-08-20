a98b44a58d66220036a45805976268cb398f690e
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-20 23:54:41 +0200

    Update config.sh

--- lxc/emby/config.sh
+++ lxc/emby/config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-Commands="pct exec $ctID -- bash -ci \"wget -q https://github.com/MediaBrowser/Emby.Releases/releases/download/$( githubLatest \"MediaBrowser/Emby.Releases\" )/emby-server-deb_$( githubLatest \"MediaBrowser/Emby.Releases\" )_amd64.deb\"
-          pct exec $ctID -- bash -ci \"dpkg -i emby-server-deb_*_amd64.deb > /dev/null 2>&1\"
-          pct exec $ctID -- bash -ci \"rm emby-server-deb_*_amd64.deb\"
-          pct exec $ctID -- bash -ci \"systemctl start emby-server && systemctl enable emby-server > /dev/null 2>&1\""
+pct exec $1 -- bash -ci "wget -q https://github.com/MediaBrowser/Emby.Releases/releases/download/$( githubLatest \"MediaBrowser/Emby.Releases\" )/emby-server-deb_$( githubLatest \"MediaBrowser/Emby.Releases\" )_amd64.deb"
+pct exec $1 -- bash -ci "dpkg -i emby-server-deb_*_amd64.deb > /dev/null 2>&1"
+pct exec $1 -- bash -ci "rm emby-server-deb_*_amd64.deb"
+pct exec $1 -- bash -ci "systemctl start emby-server && systemctl enable emby-server > /dev/null 2>&1"
+
+exit 0
