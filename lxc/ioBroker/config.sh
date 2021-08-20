535fdd29c67d770b1010e338a789612d4d0d69ba
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-20 23:54:56 +0200

    Update config.sh

--- lxc/ioBroker/config.sh
+++ lxc/ioBroker/config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-Commands="pct exec $ctID -- bash -ci \"curl -sLf https://iobroker.net/install.sh | bash - > /dev/null 2>&1\"
-          iobinstallVariation"
+pct exec $1 -- bash -ci "curl -sLf https://iobroker.net/install.sh | bash - > /dev/null 2>&1"
+iobinstallVariation
+
+exit 0
