810deb53ae21565073d18244fa6239bab237d83e
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-21 01:53:34 +0200

    Update config.sh

--- lxc/Radarr/config.sh
+++ lxc/Radarr/config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ctID=$1
-ctRootpw=$2
-ctIP=$(lxc-info $ctID -iH | grep $networkIP)
-containername=$(pct list | grep 100 | awk '{print $3}')
-
 source "$script_path/bin/variables.sh"
 source "$script_path/handler/global_functions.sh"
 source "$shiot_configPath/$shiot_configFile"
 source "$script_path/language/$var_language.sh"
 source "$script_path/lxc/$containername/language/$var_language.sh"
+
+ctID=$1
+ctRootpw=$2
+ctIP=$(lxc-info $ctID -iH | grep $networkIP)
+containername=$(pct list | grep 100 | awk '{print $3}')
 
 pct exec $ctID -- bash -ci "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 pct exec $ctID -- bash -ci "echo \"deb https://download.mono-project.com/repo/ubuntu stable-focal main\" | tee /etc/apt/sources.list.d/mono-official-stable.list"
