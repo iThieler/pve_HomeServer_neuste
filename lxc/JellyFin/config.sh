0428b48ece689ca45163877efff872b789ca33b3
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-20 23:55:01 +0200

    Update config.sh

--- lxc/JellyFin/config.sh
+++ lxc/JellyFin/config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-Commands="pct exec $ctID -- bash -ci \"add-apt-repository universe\"
-          pct exec $ctID -- bash -ci \"wget -O - https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | apt-key add - > /dev/null 2>&1\"
-          pct exec $ctID -- bash -ci \"echo \"deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/ubuntu $( lsb_release -c -s ) main\" | tee /etc/apt/sources.list.d/jellyfin.list > /dev/null 2>&1\"
-          pct exec $ctID -- bash -ci \"apt-get install -y jellyfin > /dev/null 2>&1\"
-          pct exec $ctID -- bash -ci \"mkdir -p /media/Movies/\"
-          pct exec $ctID -- bash -ci \"mkdir -p /media/Series/\"
-          pct exec $ctID -- bash -ci \"mkdir -p /media/Photos/\"
-          pct exec $ctID -- bash -ci \"systemctl start jellyfin && systemctl enable jellyfin > /dev/null 2>&1\""
+pct exec $1 -- bash -ci "add-apt-repository universe"
+pct exec $1 -- bash -ci "wget -O - https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | apt-key add - > /dev/null 2>&1"
+pct exec $1 -- bash -ci "echo \"deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/ubuntu $( lsb_release -c -s ) main\" | tee /etc/apt/sources.list.d/jellyfin.list > /dev/null 2>&1"
+pct exec $1 -- bash -ci "apt-get install -y jellyfin > /dev/null 2>&1"
+pct exec $1 -- bash -ci "mkdir -p /media/Movies/"
+pct exec $1 -- bash -ci "mkdir -p /media/Series/"
+pct exec $1 -- bash -ci "mkdir -p /media/Photos/"
+pct exec $1 -- bash -ci "systemctl start jellyfin && systemctl enable jellyfin > /dev/null 2>&1"
+
+exit 0
