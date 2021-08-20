fd73d3b21cf725cc601d3b5f6681cb9ba80fe3d5
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-20 23:54:46 +0200

    Update config.sh

--- lxc/Heimdall/config.sh
+++ lxc/Heimdall/config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-Commands="pct exec $ctID -- bash -ci \"apt-get install -y docker.io docker-compose > /dev/null 2>&1\"
-          pct exec $ctID -- bash -ci \"systemctl start docker && systemctl enable docker > /dev/null 2>&1\"
-          pct exec $ctID -- bash -ci \"mkdir -p /root/heimdall/\"
-          pct push $ctIP \"$script_path/lxc/heimdall/docker-compose.yml\" \"/root/heimdall/docker-compose.yml\"
-          pct exec $ctID -- bash -ci \"sed -i 's#TIMEZONETOCHANGE#'"$timezone"'#' /root/heimdall/docker-compose.yml\"
-          pct exec $ctID -- bash -ci \"cd /root/heimdall && docker-compose up -d --quiet-pull > /dev/null 2>&1\""
+pct exec $1 -- bash -ci "apt-get install -y docker.io docker-compose > /dev/null 2>&1"
+pct exec $1 -- bash -ci "systemctl start docker && systemctl enable docker > /dev/null 2>&1"
+pct exec $1 -- bash -ci "mkdir -p /root/heimdall/"
+pct push $1 "$script_path/lxc/heimdall/docker-compose.yml" "/root/heimdall/docker-compose.yml"
+pct exec $1 -- bash -ci "sed -i 's#TIMEZONETOCHANGE#'"$timezone"'#' /root/heimdall/docker-compose.yml"
+pct exec $1 -- bash -ci "cd /root/heimdall && docker-compose up -d --quiet-pull > /dev/null 2>&1"
+
+exit 0
