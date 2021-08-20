e9c1e95acf93e162e8ff5bb033df99472a8a9d18
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-20 23:55:34 +0200

    Update config.sh

--- lxc/Sonarr/config.sh
+++ lxc/Sonarr/config.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-Commands="pct exec $ctID -- bash -ci \"apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493\"
-          pct exec $ctID -- bash -ci \"echo \"deb http://apt.sonarr.tv/ master main\" | tee /etc/apt/sources.list.d/sonarr.list\"
-          pct exec $ctID -- bash -ci \"apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF\"
-          pct exec $ctID -- bash -ci \"echo \"deb https://download.mono-project.com/repo/ubuntu stable-focal main\" | tee /etc/apt/sources.list.d/mono-official-stable.list\"
-          pct exec $ctID -- bash -ci \"apt-get install -y mono-devel mediainfo nzbdrone > /dev/null 2>&1\"
-          pct exec $ctID -- bash -ci \"mkdir -p /media/Series/ > /dev/null 2>&1\"
-          pct push $ctIP \"$script_path/lxc/Sonarr/sonarr.service\" /etc/systemd/system/sonarr.service\"
-          pct push $ctIP \"$script_path/lxc/Sonarr/config.xml\" \"/root/.config/NzbDrone/config.xml\"
-          pct exec $ctID -- bash -ci \"sed -i 's#IPADRESSTOCHANGE#'"$ctIP"'#' /root/.config/NzbDrone/config.xml\"
-          pct exec $ctID -- bash -ci \"sed -i 's#APIKEYTOCHANGE#'"$( createAPIKey 32 )"'#' /root/.config/NzbDrone/config.xml\"
-          pct exec $ctID -- bash -ci \"systemctl start sonarr && systemctl enable sonarr\""
+pct exec $1 -- bash -ci "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493"
+pct exec $1 -- bash -ci "echo \"deb http://apt.sonarr.tv/ master main\" | tee /etc/apt/sources.list.d/sonarr.list"
+pct exec $1 -- bash -ci "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+pct exec $1 -- bash -ci "echo \"deb https://download.mono-project.com/repo/ubuntu stable-focal main\" | tee /etc/apt/sources.list.d/mono-official-stable.list"
+pct exec $1 -- bash -ci "apt-get install -y mono-devel mediainfo nzbdrone > /dev/null 2>&1"
+pct exec $1 -- bash -ci "mkdir -p /media/Series/ > /dev/null 2>&1"
+pct push $1 "$script_path/lxc/Sonarr/sonarr.service" "/etc/systemd/system/sonarr.service"
+pct push $1 "$script_path/lxc/Sonarr/config.xml" "/root/.config/NzbDrone/config.xml"
+pct exec $1 -- bash -ci "sed -i 's#IPADRESSTOCHANGE#'"$2"'#' /root/.config/NzbDrone/config.xml"
+pct exec $1 -- bash -ci "sed -i 's#APIKEYTOCHANGE#'"$( createAPIKey 32 )"'#' /root/.config/NzbDrone/config.xml"
+pct exec $1 -- bash -ci "systemctl start sonarr && systemctl enable sonarr"
+
+exit 0
