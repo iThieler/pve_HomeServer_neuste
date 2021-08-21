2bd5b233ddd121e39f34ac52fb97701b7d9a836d
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-21 11:42:48 +0200

    Update config.sh

--- lxc/JellyFin/config.sh
+++ lxc/JellyFin/config.sh
@@ -4,15 +4,16 @@ source "$script_path/bin/variables.sh"
 source "$script_path/handler/global_functions.sh"
 source "$shiot_configPath/$shiot_configFile"
 source "$script_path/language/$var_language.sh"
-source "$script_path/lxc/$containername/language/$var_language.sh"
 
 ctID=$1
 ctRootpw=$2
 ctIP=$(lxc-info $ctID -iH | grep $networkIP)
-containername=$(pct list | grep 100 | awk '{print $3}')
+containername=$(pct list | grep $ctID | awk '{print $3}')
+
+source "$script_path/lxc/$containername/language/$var_language.sh"
 
 pct exec $ctID -- bash -ci "add-apt-repository universe"
-pct exec $ctID -- bash -ci "wget -O - https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | apt-key add - > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "wget -qO - https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | apt-key add - > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "echo \"deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/ubuntu $( lsb_release -c -s ) main\" | tee /etc/apt/sources.list.d/jellyfin.list > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "apt-get install -y jellyfin > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "mkdir -p /media/Movies/"
