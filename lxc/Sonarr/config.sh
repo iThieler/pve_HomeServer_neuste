43a5589fb43ced79c1ad12c756c2a9120cc3bd1f
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-21 11:43:26 +0200

    Update config.sh

--- lxc/Sonarr/config.sh
+++ lxc/Sonarr/config.sh
@@ -4,23 +4,24 @@ source "$script_path/bin/variables.sh"
 source "$script_path/handler/global_functions.sh"
 source "$shiot_configPath/$shiot_configFile"
 source "$script_path/language/$var_language.sh"
-source "$script_path/lxc/$containername/language/$var_language.sh"
 
 ctID=$1
 ctRootpw=$2
 ctIP=$(lxc-info $ctID -iH | grep $networkIP)
-containername=$(pct list | grep 100 | awk '{print $3}')
+containername=$(pct list | grep $ctID | awk '{print $3}')
 
-pct exec $ctID -- bash -ci "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493"
-pct exec $ctID -- bash -ci "echo \"deb http://apt.sonarr.tv/ master main\" | tee /etc/apt/sources.list.d/sonarr.list"
-pct exec $ctID -- bash -ci "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-pct exec $ctID -- bash -ci "echo \"deb https://download.mono-project.com/repo/ubuntu stable-focal main\" | tee /etc/apt/sources.list.d/mono-official-stable.list"
+source "$script_path/lxc/$containername/language/$var_language.sh"
+
+pct exec $ctID -- bash -ci "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "echo \"deb http://apt.sonarr.tv/ master main\" | tee /etc/apt/sources.list.d/sonarr.list > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "echo \"deb https://download.mono-project.com/repo/ubuntu stable-focal main\" | tee /etc/apt/sources.list.d/mono-official-stable.list > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "apt-get install -y mono-devel mediainfo nzbdrone > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "mkdir -p /media/Series/ > /dev/null 2>&1"
-pct push $ctID "$script_path/lxc/Sonarr/sonarr.service" "/etc/systemd/system/sonarr.service"
-pct push $ctID "$script_path/lxc/Sonarr/config.xml" "/root/.config/NzbDrone/config.xml"
+pct push $ctID "$script_path/lxc/$containername/sonarr.service" "/etc/systemd/system/sonarr.service"
+pct push $ctID "$script_path/lxc/$containername/config.xml" "/root/.config/NzbDrone/config.xml"
 pct exec $ctID -- bash -ci "sed -i 's#IPADRESSTOCHANGE#'"$ctIP"'#' /root/.config/NzbDrone/config.xml"
-pct exec $ctID -- bash -ci "sed -i 's#APIKEYTOCHANGE#'"$( createAPIKey 32 )"'#' /root/.config/NzbDrone/config.xml"
+pct exec $ctID -- bash -ci "sed -i 's#APIKEYTOCHANGE#'"$( generateAPIKey 32 )"'#' /root/.config/NzbDrone/config.xml"
 pct exec $ctID -- bash -ci "systemctl start sonarr && systemctl enable sonarr"
 
 exit 0
