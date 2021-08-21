a18bd4605cd41971a22cad256d373d3fa0c834fa
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-21 11:42:32 +0200

    Update config.sh

--- lxc/Heimdall/config.sh
+++ lxc/Heimdall/config.sh
@@ -4,17 +4,18 @@ source "$script_path/bin/variables.sh"
 source "$script_path/handler/global_functions.sh"
 source "$shiot_configPath/$shiot_configFile"
 source "$script_path/language/$var_language.sh"
-source "$script_path/lxc/$containername/language/$var_language.sh"
 
 ctID=$1
 ctRootpw=$2
 ctIP=$(lxc-info $ctID -iH | grep $networkIP)
-containername=$(pct list | grep 100 | awk '{print $3}')
+containername=$(pct list | grep $ctID | awk '{print $3}')
+
+source "$script_path/lxc/$containername/language/$var_language.sh"
 
 pct exec $ctID -- bash -ci "apt-get install -y docker.io docker-compose > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "systemctl start docker && systemctl enable docker > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "mkdir -p /root/heimdall/"
-pct push $ctID "$script_path/lxc/heimdall/docker-compose.yml" "/root/heimdall/docker-compose.yml"
+pct push $ctID "$script_path/lxc/$containername/docker-compose.yml" "/root/heimdall/docker-compose.yml"
 pct exec $ctID -- bash -ci "sed -i 's#TIMEZONETOCHANGE#'"$timezone"'#' /root/heimdall/docker-compose.yml"
 pct exec $ctID -- bash -ci "cd /root/heimdall && docker-compose up -d --quiet-pull > /dev/null 2>&1"
 
