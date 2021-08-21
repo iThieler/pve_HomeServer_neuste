53e3a8ecb84c4256789834f79e8dfb2f3081ba05
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-21 20:10:45 +0200

    Insert new Head

--- lxc/_AdGuard/config.sh
+++ lxc/_AdGuard/config.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-##################### Container Commands ######################
+source "$script_path/bin/variables.sh"
+source "$script_path/handler/global_functions.sh"
+source "$shiot_configPath/$shiot_configFile"
+source "$script_path/language/$var_language.sh"
 
-lxcCommands=""
+ctID=$1
+ctRootpw=$2
+ctIP=$(lxc-info $ctID -iH | grep $networkIP)
+containername=$(pct list | grep $ctID | awk '{print $3}')
 
-######################## Host Commands ########################
+# Load container language file if not exist load english language
+if [ -f "$script_path/lxc/$containername/language/$var_language.sh" ]; then
+  source "$script_path/lxc/$containername/language/$var_language.sh"
+else
+  source "$script_path/lxc/$containername/language/en.sh"
+fi
 
-pveCommands=""
+
+
+exit 0
--- lxc/_ecoDMS/config.sh
+++ lxc/_ecoDMS/config.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+source "$script_path/bin/variables.sh"
+source "$script_path/handler/global_functions.sh"
+source "$shiot_configPath/$shiot_configFile"
+source "$script_path/language/$var_language.sh"
+
+ctID=$1
+ctRootpw=$2
+ctIP=$(lxc-info $ctID -iH | grep $networkIP)
+containername=$(pct list | grep $ctID | awk '{print $3}')
+
+# Load container language file if not exist load english language
+if [ -f "$script_path/lxc/$containername/language/$var_language.sh" ]; then
+  source "$script_path/lxc/$containername/language/$var_language.sh"
+else
+  source "$script_path/lxc/$containername/language/en.sh"
+fi
+
+
+
+exit 0
