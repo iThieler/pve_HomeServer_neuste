865f4a28c3e2aef75a959cdef314bfb55d984df2
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-21 23:30:26 +0200

    Update generate_lxc.sh

--- handler/generate_lxc.sh
+++ handler/generate_lxc.sh
@@ -13,7 +13,7 @@ echo "- ${txt_0201}"
 available_lxc=$(find $script_path/lxc/* -prune -type d ! -path "$script_path/lxc/_*" | while IFS= read -r d; do echo -e "$d"; done | sed -e "s#$script_path/lxc/##g" | sed ':M;N;$!bM;s#\n# #g')
 echo -e "#!/bin/bash\n\nlxc_list=( \\" > /tmp/lxclist.sh
 desc="desc_${var_language}"
-if [ -z "${!desc}" ]; then desc="desc_en"; fi 
+if [ -n "${!desc}" ]; then desc="desc_en"; fi 
 for lxc in $available_lxc; do
   source "$script_path/lxc/${lxc}/description.sh"
   if [ -z "$var_nasip" ] && ! $nasonly; then
