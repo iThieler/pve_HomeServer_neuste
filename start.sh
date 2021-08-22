35cc35acf092110e4c0841d3bb0b9582fd4c24b9
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-22 22:16:25 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -26,7 +26,7 @@ fi
 
 # Checks the PVE MajorRelease
 pve_majorversion=$(pveversion | cut -d/ -f2 | cut -d. -f1)
-if [ $pve_majorversion -ne 6 ] || [ $pve_majorversion -ne 7 ]; then
+if [ "$pve_majorversion" -lt 6 ]; then
   echo "- This script currently works only for Proxmox version 6.X or version 7.X"
   exit 1
 fi
