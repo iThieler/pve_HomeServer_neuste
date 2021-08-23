e535c17a55fd5db07a1c9e38a1f98356d5b579ed
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-23 18:20:23 +0200

    Update config.sh

--- lxc/3cx/config.sh
+++ lxc/3cx/config.sh
@@ -5,6 +5,13 @@ source "$script_path/handler/global_functions.sh"
 source "$shiot_configPath/$shiot_configFile"
 source "$script_path/language/$var_language.sh"
 
+# Load container language file if not exist load english language
+if [ -f "$script_path/lxc/$containername/language/$var_language.sh" ]; then
+  source "$script_path/lxc/$containername/language/$var_language.sh"
+else
+  source "$script_path/lxc/$containername/language/en.sh"
+fi
+
 ctID=$1
 ctRootpw=$2
 ctIP=$(lxc-info $ctID -iH | grep $networkIP)
@@ -19,6 +26,7 @@ fi
 
 pct exec $ctID -- bash -ci "wget -qO - http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add - > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "echo \"deb http://downloads-global.3cx.com/downloads/debian stretch main\" | tee /etc/apt/sources.list.d/3cxpbx.list > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "apt-get update > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "apt-get install -y iperf libicu57 libicu57 dphys-swapfile 3cxpbx > /dev/null 2>&1"
 
 exit 0
