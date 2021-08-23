f9a16836623eeeb5a2dfb0821e051df082aeaf00
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-23 19:33:33 +0200

    Update description.sh

--- lxc/3cx/description.sh
+++ lxc/3cx/description.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 nasonly=false
+commands="apt-get install -y 3cxpbx"
 
 ################## Descriptions ###################
 desc_en="VoIP Phone System"
 desc_de="VoIP Telefonanlage"
+
+################## Info E-Mail ###################
+mail_en=""
+mail_de="Da bei der Installation der Telefonanlagensoftware Benutzereingaben benötigt werden, musst du dich an der Konsole einloggen und folgenden Befehl ausführen. Entschuldige die Umstände."
