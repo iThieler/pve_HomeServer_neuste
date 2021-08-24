a84c869f893ab2fb8fd615926cc539d3a49cef20
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-24 11:33:06 +0200

    Update config.sh

--- lxc/3cx/config.sh
+++ lxc/3cx/config.sh
@@ -10,7 +10,6 @@ ctRootpw=$2
 ctIP=$(lxc-info $ctID -iH | grep $networkIP)
 containername=$(pct list | grep $ctID | awk '{print $3}')
 
-echo -e "3cx Config source \"$script_path/lxc/$containername/language/$var_language.sh\""
 pct exec $ctID -- bash -ci "wget -qO - http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add - > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "echo \"deb http://downloads-global.3cx.com/downloads/debian stretch main\" | tee /etc/apt/sources.list.d/3cxpbx.list > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "apt-get update > /dev/null 2>&1"
