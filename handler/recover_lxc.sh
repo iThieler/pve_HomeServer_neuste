860af4b44b9a380c457727455e1c0d1f70d471d7
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-24 02:36:09 +0200

    Create recover_lxc.sh

---
+++ handler/recover_lxc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source "$script_path/bin/variables.sh"
+source "$script_path/bin/var_containerOS.sh"
+source "$script_path/handler/global_functions.sh"
+source "$shiot_configPath/$shiot_configFile"
+source "$script_path/language/$var_language.sh"
