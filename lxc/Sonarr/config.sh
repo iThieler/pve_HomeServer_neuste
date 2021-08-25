5115d8e46b5b6027adc072b1c054fb3c509cf227
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-26 00:26:48 +0200

    Update config.sh

--- lxc/Sonarr/config.sh
+++ lxc/Sonarr/config.sh
@@ -14,6 +14,7 @@ pct exec $ctID -- bash -ci "apt-key adv --keyserver keyserver.ubuntu.com --recv-
 pct exec $ctID -- bash -ci "echo \"deb http://apt.sonarr.tv/ master main\" | tee /etc/apt/sources.list.d/sonarr.list > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "echo \"deb https://download.mono-project.com/repo/ubuntu stable-focal main\" | tee /etc/apt/sources.list.d/mono-official-stable.list > /dev/null 2>&1"
+pct exec $ctID -- bash -ci "apt-get update > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "apt-get install -y mono-devel mediainfo nzbdrone > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "mkdir -p /media/Series/ > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "mkdir -p /root/.config/NzbDrone/ > /dev/null 2>&1"
