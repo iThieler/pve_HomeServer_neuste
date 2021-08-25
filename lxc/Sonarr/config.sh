63ec76d2b03fdaa6c9b236765d6edc26963aae42
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-26 00:48:01 +0200

    Update config.sh

--- lxc/Sonarr/config.sh
+++ lxc/Sonarr/config.sh
@@ -10,6 +10,11 @@ source "$script_path/handler/global_functions.sh"
 source "$shiot_configPath/$shiot_configFile"
 source "$script_path/language/$var_language.sh"
 
+pct shutdown $ctID --timeout 5
+sleep 10
+pct set $ctID --memory 1024 --swap 1024
+pct start $ctID
+sleep 10
 pct exec $ctID -- bash -ci "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "echo \"deb http://apt.sonarr.tv/ master main\" | tee /etc/apt/sources.list.d/sonarr.list > /dev/null 2>&1"
 pct exec $ctID -- bash -ci "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF > /dev/null 2>&1"
@@ -23,5 +28,9 @@ pct push $ctID "$script_path/lxc/$containername/config.xml" "/root/.config/NzbDr
 pct exec $ctID -- bash -ci "sed -i 's#IPADRESSTOCHANGE#'"$networkIP"'.'"$ctIP"'#' /root/.config/NzbDrone/config.xml"
 pct exec $ctID -- bash -ci "sed -i 's#APIKEYTOCHANGE#'"$( generateAPIKey 32 )"'#' /root/.config/NzbDrone/config.xml"
 pct exec $ctID -- bash -ci "systemctl start sonarr && systemctl enable sonarr > /dev/null 2>&1"
+pct shutdown $ctID --timeout 5
+sleep 10
+pct set $ctID --memory 256 --swap 256
+pct start $ctID
 
 exit 0
