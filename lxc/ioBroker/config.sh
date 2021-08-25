7a2b935664593d3cf20ce94e4db2e603ef1bff04
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-25 22:29:36 +0200

    Update config.sh

--- lxc/ioBroker/config.sh
+++ lxc/ioBroker/config.sh
@@ -4,53 +4,71 @@ ctID=$1
 ctIP=$2
 ctRootpw="$3"
 containername="$4"
+configFile="cfg_ioBroker.sh"
 
 source "$script_path/bin/variables.sh"
 source "$script_path/handler/global_functions.sh"
 source "$shiot_configPath/$shiot_configFile"
 source "$script_path/language/$var_language.sh"
+if [ -f "$shiot_configPath/$configFile" ]; then source "$shiot_configPath/$configFile"; fi
+
+# If Container Language Folder exist, load container language file if, not exist load english language
+if [ -d "$script_path/lxc/$containername/language" ]; then
+  if [ -f "$script_path/lxc/$containername/language/$var_language.sh" ]; then
+    source "$script_path/lxc/$containername/language/$var_language.sh"
+  else
+    source "$script_path/lxc/$containername/language/en.sh"
+  fi
+fi
 
 standardAdapter="parser javascript web vis vis-inventwo vis-icontwo influxdb proxmox"
-configFile="cfg_ioBroker.sh"
 
 pct exec $ctID -- bash -ci "curl -sLf https://iobroker.net/install.sh | bash - > /dev/null 2>&1"
 
 todo=(\
-          "1" "${lxc_txt_002}" \
-          "2" "${lxc_txt_003}" \
-          )
+      "1" "${lxc_txt_002}" \
+      "2" "${lxc_txt_003}" \
+)
 
 gw=(\
-  "unifi" "  Ubiquiti/UniFi DreamMachine Pro ${wrd_14} CloudKey" off \
-  "avm" "  AVM FRITZ!Box" off \
-  "none" "  ${lxc_txt_009}" off \
+    "unifi" "  Ubiquiti/UniFi DreamMachine Pro ${wrd_14} CloudKey" off \
+    "avm" "  AVM FRITZ!Box" off \
+    "none" "  ${lxc_txt_009}" off \
 )
 
 if [ -z "$var_nasip" ]; then pct exec $ctID -- bash -ci "mkdir -p /mnt/backup/$containername/javascript/"; fi
 
 variation=$(whiptail --menu --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ioBroker " "\n$lxc_txt_001" 20 80 10 "${todo[@]}" 3>&1 1>&2 2>&3)
 if [[ $variation == "1" ]]; then
-  gateway=$(whiptail --menu --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ioBroker " "\n$lxc_txt_010" 20 80 10 "${gw[@]}" 3>&1 1>&2 2>&3)
-  vislicensecode=$(whiptail --inputbox --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_005" 20 80 3>&1 1>&2 2>&3)
+  if [ -z "$gateway" ]; then
+    gateway=$(whiptail --menu --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ioBroker " "\n$lxc_txt_010" 20 80 10 "${gw[@]}" 3>&1 1>&2 2>&3)
+  fi
+  if [ -z "$vislicensecode" ]; then
+    vislicensecode=$(whiptail --inputbox --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_005" 20 80 3>&1 1>&2 2>&3)
+  fi
   if [ $(pct list | grep -cw \"iDBGrafana\") -eq 0 ]; then
-    grafanaPW=$(whiptail --inputbox --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ioBroker " "Wie lautet dein Grafana Passwort? (leer = Skript Standardpasswort)" 20 80 3>&1 1>&2 2>&3)
-    if [ -n "$grafanaPW" ]; then grafanaPW="changeme"; fi
+    if [ -z "$grafanaPW" ]; then
+      grafanaPW=$(whiptail --inputbox --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ioBroker " "Wie lautet dein Grafana Passwort? (leer = Skript Standardpasswort)" 20 80 3>&1 1>&2 2>&3)
+      if [ -n "$grafanaPW" ]; then grafanaPW="changeme"; fi
+    fi
     grafana=" --influxDBEnabled true --grafanaEnabled true --grafanaHost $(lxc-info $(pct list | grep iDBGrafana | awk '{print $1}') -iH) --grafanaPassword \"${grafanaPW}\" "
   fi
   if [ -z "$var_nasip" ]; then
     nas=" --cifsEnabled true --cifsMount $var_nasip --cifsUser $var_robotname --cifsPassword $var_robotpw --cifsDir \"/mnt/backup/$containername/\" --javascriptsPath \"/mnt/backup/$containername/javascript/\""
   fi
-  pct exec $ctID -- bash -ci "iobroker set backitup.0 --minimalEnabled true --javascriptsEnabled true  --minimalTime \"00:00\" --minimalDeleteAfter \"6\" --select-options-abad7d88-51a8-8592-4f1f-5d1f89c614311 true${nas}${grafana} > /dev/null 2>&1"
+  pct exec $ctID -- bash -ci "iobroker set backitup.0 --minimalEnabled true --javascriptsEnabled true --minimalTime \"00:00\" --minimalDeleteAfter \"6\" --select-options-abad7d88-51a8-8592-4f1f-5d1f89c614311 true${nas}${grafana} > /dev/null 2>&1"
   for adp in $standardAdapter; do
     pct exec $ctID -- bash -ci "iobroker add iobroker.$adp > /dev/null 2>&1"
   done
+  pct exec $ctID -- bash -ci "iobroker set proxmox.0 --ip $pveIP --name $var_robotname --select-options-225a0870-9529-3d5e-9970-0bbdb092c4d21 true --pwd $var_robotpw > /dev/null 2>&1"
   if [ -z "$var_nasip" ]; then javamirrorpath=" --mirrorPath \"/mnt/backup/$containername/javascript/\""; fi
   pct exec $ctID -- bash -ci "iobroker set javascript.0$javamirrorpath --enableSetObject true --enableExec true --enableSendToHost true > /dev/null 2>&1"
-  if [ -z "$vislicensecode" ]; then pct exec $ctID -- bash -ci "iobroker set vis.0 --license "$vislicensecode" > /dev/null 2>&1"; fi
+  if [ -z "$vislicensecode" ]; then
+    pct exec $ctID -- bash -ci "iobroker set vis.0 --license "$vislicensecode" > /dev/null 2>&1"
+  fi
   if [ $(pct list | grep -cw \"iDBGrafana\") -eq 0 ]; then
     pct exec $ctID -- bash -ci "iobroker set influxdb.0 --host $(lxc-info $(pct list | grep iDBGrafana | awk '{print $1}') -iH) --user iobroker --password $ctRootpw --passwordConfirm $ctRootpw --dbname iobroker > /dev/null 2>&1"
   fi
-  pct exec $ctID -- bash -ci "iobroker set proxmox.0 --ip $pveIP --name $var_robotname --select-options-225a0870-9529-3d5e-9970-0bbdb092c4d21 true --pwd $var_robotpw > /dev/null 2>&1"
   if [[ $gateway == "avm" ]]; then
     pct exec $ctID -- bash -ci "iobroker add iobroker.fb-checkpresence --enabled > /dev/null 2>&1"
     whiptail --yesno --yes-button " ${btn_3} " --no-button " ${btn_4} " --backtitle "SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_006" 20 80
@@ -58,8 +76,12 @@ if [[ $variation == "1" ]]; then
     if [[ $gwnetrobot == 0 ]]; then
       pct exec $ctID -- bash -ci "iobroker set fb-checkpresence.0 --ipaddress $gatewayIP --username $var_robotname --password $var_robotpw --select-options-0060129c-6cb9-01e2-39c5-1449a75d940c1 true --dateformat \"dd.mm.yyyy HH:MM\" > /dev/null 2>&1"
     else
-      vargwadmin=$(whiptail --inputbox --nocancel --backtitle "SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_007" 20 80 3>&1 1>&2 2>&3)
-      vargwadminpw=$(whiptail --inputbox --nocancel --backtitle "SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_008" 20 80 3>&1 1>&2 2>&3)
+      if [ -z "$vargwadmin" ]; then
+        vargwadmin=$(whiptail --inputbox --nocancel --backtitle "SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_007" 20 80 3>&1 1>&2 2>&3)
+      fi
+      if [ -z "$vargwadminpw" ]; then
+        vargwadminpw=$(whiptail --inputbox --nocancel --backtitle "SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_008" 20 80 3>&1 1>&2 2>&3)
+      fi
       pct exec $ctID -- bash -ci "iobroker set fb-checkpresence.0 --ipaddress $gatewayIP --username $vargwadmin --password $vargwadminpw --select-options-0060129c-6cb9-01e2-39c5-1449a75d940c1 true --dateformat \"dd.mm.yyyy HH:MM\" > /dev/null 2>&1"
     fi
   elif [[ $gateway == "unifi" ]]; then
@@ -69,8 +91,12 @@ if [[ $variation == "1" ]]; then
     if [[ $yesno == 0 ]]; then
       pct exec $ctID -- bash -ci "iobroker set unifi.0 --controllerIp $gatewayIP --controllerUsername $var_robotname --controllerPassword $var_robotpw > /dev/null 2>&1"
     else
-      vargwadmin=$(whiptail --inputbox --nocancel --backtitle "SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_007" 20 80 3>&1 1>&2 2>&3)
-      vargwadminpw=$(whiptail --inputbox --nocancel --backtitle "SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_008" 20 80 3>&1 1>&2 2>&3)
+      if [ -z "$vargwadmin" ]; then
+        vargwadmin=$(whiptail --inputbox --nocancel --backtitle "SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_007" 20 80 3>&1 1>&2 2>&3)
+      fi
+      if [ -z "$vargwadminpw" ]; then
+        vargwadminpw=$(whiptail --inputbox --nocancel --backtitle "SmartHome-IoT.net" --title " ioBroker " "$lxc_txt_008" 20 80 3>&1 1>&2 2>&3)
+      fi
       pct exec $ctID -- bash -ci "iobroker set unifi.0 --controllerIp $gatewayIP --controllerUsername $vargwadmin --controllerPassword $vargwadminpw > /dev/null 2>&1"
     fi
   fi
@@ -87,25 +113,29 @@ if [[ $variation == "1" ]]; then
   pct exec $ctID -- bash -ci "iobroker set admin.0 --auth true > /dev/null 2>&1"
 elif [[ $variation == "2" ]]; then
   if [ $(pct list | grep -cw \"iDBGrafana\") -eq 0 ]; then
-    grafanaPW=$(whiptail --inputbox --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ioBroker " "Wie lautet dein Grafana Passwort? (leer = Skript Standardpasswort)" 20 80 3>&1 1>&2 2>&3)
-    if [ -z "$grafanaPW" ]; then grafanaPW="changeme"; fi
+    if [ -z "$grafanaPW" ]; then
+      grafanaPW=$(whiptail --inputbox --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ioBroker " "Wie lautet dein Grafana Passwort? (leer = Skript Standardpasswort)" 20 80 3>&1 1>&2 2>&3)
+      if [ -z "$grafanaPW" ]; then grafanaPW="changeme"; fi
+    fi
     grafana=" --influxDBEnabled true --grafanaEnabled true --grafanaHost $(lxc-info $(pct list | grep iDBGrafana | awk '{print $1}') -iH) --grafanaPassword \"${grafanaPW}\" "
   fi
   if [ -n "$var_nasip" ]; then
     nas=" --cifsEnabled true --cifsMount $var_nasip --cifsUser $var_robotname --cifsPassword $var_robotpw --cifsDir \"/mnt/backup/$containername/\" --javascriptsPath \"/mnt/backup/$containername/javascript/\""
   fi
-  pct exec $ctID -- bash -ci "iobroker set backitup.0 --minimalEnabled true --javascriptsEnabled true  --minimalTime \"00:00\" --minimalDeleteAfter \"6\" --select-options-abad7d88-51a8-8592-4f1f-5d1f89c614311 true${nas}${grafana} > /dev/null 2>&1"
+  pct exec $ctID -- bash -ci "iobroker set backitup.0 --minimalEnabled true --javascriptsEnabled true --minimalTime \"00:00\" --minimalDeleteAfter \"6\" --select-options-abad7d88-51a8-8592-4f1f-5d1f89c614311 true${nas}${grafana} > /dev/null 2>&1"
 
   # create config File
   echo -e "\0043\0041/bin/bash" > $shiot_configPath/$configFile
   echo -e "\0043 NOTICE: Backup ioBroker Configuration Script from SmartHome-IoT.net" >> $shiot_configPath/$configFile
   echo -e "\0043 Created on $(date)" >> $shiot_configPath/$configFile
 
+  echo -e "\n\0043 ioBroker Adapter configuration" >> $shiot_configPath/$configFile
+  echo -e "vislicensecode=\"$vislicensecode\"" >> $shiot_configPath/$configFile
+  echo -e "grafanaPW=\"$grafanaPW\"" >> $shiot_configPath/$configFile
+
   echo -e "\n\0043 Network Hardware configuration" >> $shiot_configPath/$configFile
   echo -e "gateway=\"$gateway\"" >> $shiot_configPath/$configFile
   echo -e "synology=\"$synology\"" >> $shiot_configPath/$configFile
-
-  echo -e "\n\0043 User configuration" >> $shiot_configPath/$configFile
   if [[ $gwnetrobot == 0 ]]; then
     echo -e "vargwadmin=\"$var_robotname\"" >> $shiot_configPath/$configFile
     echo -e "vargwadminpw=\"$var_robotpw\"" >> $shiot_configPath/$configFile
