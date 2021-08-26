b290e12540006b655454c826636fdc9f536029d9
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-26 02:37:47 +0200

    Update config.sh

--- lxc/0_template/config.sh
+++ lxc/0_template/config.sh
@@ -10,6 +10,15 @@ source "$script_path/handler/global_functions.sh"          # Load global Functio
 source "$shiot_configPath/$shiot_configFile"               # Load saved configuration variables
 source "$script_path/language/$var_language.sh"            # Load global Language File
 
+# If Container Language Folder exist, load container language file if, not exist load english language    #
+#if [ -d "$script_path/lxc/$containername/language" ]; then                                               #
+#  if [ -f "$script_path/lxc/$containername/language/$var_language.sh" ]; then                            # If custom texts are needed for the container
+#    source "$script_path/lxc/$containername/language/$var_language.sh"                                   # configuration, create a folder named "language"
+#  else                                                                                                   # in this folder a file named en.sh must be created
+#    source "$script_path/lxc/$containername/language/en.sh"                                              #
+#  fi                                                                                                     #
+#fi                                                                                                       #
+
 ### Start with Commands and functions
 # Commands are executed on the Proxmox host. To execute a command on the container, use the
 # pct exec $ctID -- bash -ci ""
