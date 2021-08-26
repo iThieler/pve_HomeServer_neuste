bd7ef21428c743e23c0f210071c6f2fcc5e2da59
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-26 02:38:05 +0200

    Update description.sh

--- lxc/_nextCloud/description.sh
+++ lxc/_nextCloud/description.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-nasonly=true
+nasonly=true                                      # ture or false
 
 ################## Descriptions ###################
-desc_en="Build your own Cloud Server"
-desc_de="Baue deinen eigenen Cloudserver"
+desc_en="Containerdescription"                    # is needed
+desc_de="Containerbeschreibung"
+
+################### Info E-Mail ###################
+commandsAfterCFG="apt-get install -y XXX"         # Commands that the user must execute, line break with \n
+                                                  # If the variable is empty, no e-mail will be sent
+
+mail_en="Text to send per e-mail"                 # is needed
+mail_de="Text der per E-Mail versendet werden soll"
