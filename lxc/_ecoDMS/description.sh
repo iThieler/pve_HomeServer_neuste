bd947337601094d08323692d4d1dc5fff7d7d1bf
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-26 02:37:54 +0200

    Update description.sh

--- lxc/_ecoDMS/description.sh
+++ lxc/_ecoDMS/description.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-nasonly=true
+nasonly=true                                      # ture or false
 
 ################## Descriptions ###################
-desc_en="Document Management System"
-desc_de="Dokumentenmanagementsystem"
+desc_en="Containerdescription"                    # is needed
+desc_de="Containerbeschreibung"
+
+################### Info E-Mail ###################
+commandsAfterCFG="apt-get install -y XXX"         # Commands that the user must execute, line break with \n
+                                                  # If the variable is empty, no e-mail will be sent
+
+mail_en="Text to send per e-mail"                 # is needed
+mail_de="Text der per E-Mail versendet werden soll"
