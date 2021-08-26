be0dfe52dba89fc4ea0c72bf05d65527215e0056
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-26 02:38:40 +0200

    Update description.sh

--- lxc/_unifi/description.sh
+++ lxc/_unifi/description.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-nasonly=false
+nasonly=true                                      # ture or false
 
 ################## Descriptions ###################
-desc_en="Configure your Ubiquiti devices"
-desc_de="Konfiguriere deine Ubiquiti Geräte"
+desc_en="Containerdescription"                    # is needed
+desc_de="Containerbeschreibung"
+
+################### Info E-Mail ###################
+commandsAfterCFG="apt-get install -y XXX"         # Commands that the user must execute, line break with \n
+                                                  # If the variable is empty, no e-mail will be sent
+
+mail_en="Text to send per e-mail"                 # is needed
+mail_de="Text der per E-Mail versendet werden soll"
