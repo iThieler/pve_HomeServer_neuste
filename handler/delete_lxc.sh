c04e826666de047d071c8ef2863973bc93b961fb
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-08-29 17:05:43 +0200

    Update delete_lxc.sh

--- handler/delete_lxc.sh
+++ handler/delete_lxc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+var_language=$1
 script_path=$(realpath "$0" | sed 's|\(.*\)/.*|\1|' | cut -d/ -f1,2,3)
 
 source "$script_path/helper/variables.sh"
@@ -8,21 +9,21 @@ source "$shiot_configPath/$shiot_configFile"
 source "$script_path/language/$var_language.sh"
 
 # make list of available Containers, hide already existing
-echo "- ${txt_0201}"
+echoLOG b "${txt_0501}"
 available_lxc=$(pct list | awk '{print $1}' | tail +2 | sed ':M;N;$!bM;s#\n# #g')
 echo -e "#!/bin/bash\n\nlxc_list=( \\" > /tmp/lxclist.sh
-desc="desc_${var_language}"
-if [ -n "${!desc}" ]; then desc="desc_en"; fi 
 for lxc in $available_lxc; do
   lxc=$(pct list | grep -w ${lxc} | awk '{print $3}')
   source "$script_path/lxc/${lxc}/description.sh"
+  desc="desc_${var_language}"
+  if [ -n "${!desc}" ]; then desc="desc_en"; fi 
   echo -e "\"${lxc}\" \""${!desc}  "\" off \\" >> /tmp/lxclist.sh
 done
 echo -e ")" >> /tmp/lxclist.sh
 
 source /tmp/lxclist.sh
 
-var_lxcchoice=$(whiptail --checklist --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ${tit_6} " "\n${txt_0206}" 20 80 15 "${lxc_list[@]}" 3>&1 1>&2 2>&3 | sed 's#"##g')
+var_lxcchoice=$(whiptail --checklist --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " ${tit_0005} " "\n${txt_0502}" 20 80 15 "${lxc_list[@]}" 3>&1 1>&2 2>&3 | sed 's#"##g')
 
 for choosed_lxc in $var_lxcchoice; do
   ctID=$(pct list | grep -w "$choosed_lxc" | awk '{print $1}')
@@ -32,15 +33,16 @@ for choosed_lxc in $var_lxcchoice; do
     textbox=white,red
     button=black,yellow
   ' \
-  whiptail --yesno --yes-button " ${btn_3} " --no-button " ${btn_4} " --backtitle "© 2021 - SmartHome-IoT.net" --title " ${tit_8} " "\n${txt_0208}\n\n${wrd_7}: ${ctID}\n${wrd_6}: ${choosed_lxc}\n\n${txt_0209}" 15 80
+  whiptail --yesno --yes-button " ${btn_3} " --no-button " ${btn_4} " --backtitle "© 2021 - SmartHome-IoT.net" --title " ${txt_0502} " "\n${txt_0503}\n\n${wrd_0001}: ${ctID}\n${wrd_0002}: ${choosed_lxc}\n\n${txt_0504}" 15 80
   yesno=$?
   if [ $yesno -eq 0 ]; then
+    pct stop $ctID --skiplock 1
     pct destroy $ctID --force 1 --purge 1 > /dev/null 2>&1
     ##################################################################
     ############# Delete firewall rules of the container #############
     ##################################################################
     sleep 5
-    echo -e "- ${txt_0210}:\n  ${wrd_7}: $ctID\n  ${wrd_6}: ${choosed_lxc}"
+    echoLOG g "${txt_0505} >> ${wrd_0001}: $ctID  ${wrd_0002}: ${choosed_lxc}"
   fi
 done
 
