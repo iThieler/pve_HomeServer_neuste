a6a8a3c05ccf44f839609734ea09e5b086dc3a2b
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-09-04 22:18:01 +0200

    Update restore.sh

--- handler/restore.sh
+++ handler/restore.sh
@@ -24,45 +24,19 @@ yesno=$?
 if [ $yesno -eq 0 ]; then
   echoLOG y "${txt_1203}"
   for guest in $(ls -ldst /mnt/pve/backups/dump/*_manual.*.zst | awk '{print $10}' | cut -d- -f3); do
-    if [ $(pct list | grep -c "${guest}") -eq 1 ]; then
-      echoLOG y "${txt_1204} >> ${wrd_0001}: ${LIGHTPURPLE}${guest}${NOCOLOR}  ${wrd_0002}: ${LIGHTPURPLE}$(pct list | grep ${guest} | awk '{print $2}')${NOCOLOR}"
-      if bash "$script_path/handler/delete_lxc.sh" $var_language ${guest}; then
-        echoLOG g "${1205}"
-        if [ $(pct restore ${guest} /mnt/pve/backups/dump/*-${guest}-*_manual.tar.zst -storage ${ctTemplateDisk}) -eq 0 ]; then
-          echoLOG g "${1206}"
-        else
-          echoLOG r "${1207}"
-        fi
-      else
-        echoLOG r "${txt_1208}"
-      fi
-    elif [ $(pct list | grep -c "${guest}") -eq 0 ]; then
-      if [ $(pct restore ${guest} /mnt/pve/backups/dump/*-${guest}-*_manual.vma.zst -storage ${ctTemplateDisk}) -eq 0 ]; then
+    echoLOG y "${txt_1204} >> ${wrd_0001}: ${LIGHTPURPLE}${guest}${NOCOLOR}  ${wrd_0002}: ${LIGHTPURPLE}$(pct list | grep ${guest} | awk '{print $2}')${NOCOLOR}"
+    if [ $(ls -ldst /mnt/pve/backups/dump/*-${guest}-*_manual.*.zst | grep -c "tar") -eq 1 ]; then
+      if pct restore $guest /mnt/pve/backups/dump/*-${guest}-*_manual.tar.zst --storage ${ctTemplateDisk} --force 1; then
         echoLOG g "${1206}"
       else
         echoLOG r "${1207}"
       fi
-    elif [ $(qm list | grep -c "${guest}") -eq 1 ]; then
-      echoLOG y "${txt_1204} >> ${wrd_0001}: ${LIGHTPURPLE}${guest}${NOCOLOR}  ${wrd_0002}: ${LIGHTPURPLE}$(qm list | grep ${guest} | awk '{print $2}')${NOCOLOR}"
-      if bash "$script_path/handler/delete_vm.sh" $var_language ${guest}; then
-        echoLOG g "${1205}"
-        if [ $(pct restore ${guest} /mnt/pve/backups/dump/*-${guest}-*_manual.vma.zst -storage ${ctTemplateDisk}) -eq 0 ]; then
-          echoLOG g "${1206}"
-        else
-          echoLOG r "${1207}"
-        fi
-      else
-        echoLOG r "${txt_1208}"
-      fi
-    fi
-    elif [ $(qm list | grep -c "${guest}") -eq 0 ]; then
-      if [ $(pct restore ${guest} /mnt/pve/backups/dump/*-${guest}-*_manual.vma.zst -storage ${ctTemplateDisk}) -eq 0 ]; then
+    elif [ $(ls -ldst /mnt/pve/backups/dump/*-${guest}-*_manual.*.zst | grep -c "vma") -eq 1 ]; then
+      if qmrestore $guest /mnt/pve/backups/dump/*-${guest}-*_manual.vma.zst --storage ${ctTemplateDisk} --force 1; then
         echoLOG g "${1206}"
       else
         echoLOG r "${1207}"
       fi
-    else
-      echoLOG r "${txt_1209}"
     fi
   done
 else
