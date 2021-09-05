19e4ce45c92cd8d430c27c07302ed250d7edd56e
SmartHome-IoT <39486895+shiot@users.noreply.github.com>
2021-09-05 17:33:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -222,11 +222,11 @@ function delete() {
 }
 
 function finish() {
-  sed -i 's/[0m//g' "$shiot_configPath/$shiot_logfile"
-  sed -i 's/[1;31m//g' "$shiot_configPath/$shiot_logfile"
-  sed -i 's/[1;32m//g' "$shiot_configPath/$shiot_logfile"
-  sed -i 's/[1;33m//g' "$shiot_configPath/$shiot_logfile"
-  sed -i 's/[1;34m//g' "$shiot_configPath/$shiot_logfile"
+  sed -i 's/"[0m"/""/g' "$shiot_configPath/$shiot_logfile"
+  sed -i 's/"[1;31m"/""/g' "$shiot_configPath/$shiot_logfile"
+  sed -i 's/"[1;32m"/""/g' "$shiot_configPath/$shiot_logfile"
+  sed -i 's/"[1;33m"/""/g' "$shiot_configPath/$shiot_logfile"
+  sed -i 's/"[1;34m"/""/g' "$shiot_configPath/$shiot_logfile"
   echo -e "${txt_0037}" | mail.mailutils -a "From: \"${wrd_0006}\" <${var_senderaddress}>" -s "[SHIoT] ${wrd_0020}" "${var_rootmail}" -A "$shiot_configPath/$shiot_logfile"
   rm "$shiot_configPath/$shiot_logfile"
   unset script_path
